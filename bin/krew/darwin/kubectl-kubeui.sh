8cfeacbe64cfdbc3c5af448fc57adb9347701c96
Nick Mitchell <nickm@us.ibm.com>
2020-03-14 08:14:06 -0400

    fix: restore kubectl plugin capability
    Fixes #400

--- bin/krew/darwin/kubectl-kubeui.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-BIN=$(readlink "$0")
-SCRIPTDIR=$(cd $(dirname "$BIN") && pwd)
-
-KUI_COMMAND_CONTEXT='["kubeui","kubectl"]' "$SCRIPTDIR"/Kui-darwin-x64/Kui.app/Contents/MacOS/Kui $@ &
