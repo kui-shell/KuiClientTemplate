d652f9a9bba23ad8b544f9d238a400759b111e1c
Mengting Yan <mengting.yan1@ibm.com>
2020-03-19 14:45:20 -0400

    test: add tests for oc
    Fixes #424

--- plugins/plugin-kubeui/src/test/k8s1/apply-crd.ts
+++ plugins/plugin-kubeui/src/test/k8s1/apply-crd.ts
@@ -27,120 +27,127 @@ import {
 import { dirname } from 'path'
 const ROOT = dirname(require.resolve('@kui-shell/plugin-kubeui/tests/package.json'))
 
-describe(`kubectl apply crd ${process.env.MOCHA_RUN_TARGET || ''}`, function(this: Common.ISuite) {
-  before(Common.before(this))
-  after(Common.after(this))
+const commands = ['kubectl']
+if (process.env.NEEDS_OC) {
+  commands.push('oc')
+}
 
-  const ns: string = createNS()
-  const inNamespace = `-n ${ns}`
-  const crdName = 'crontabs.stable.example.com'
-  const kind = 'CustomResourceDefinition'
-  const apiGroup = 'apiextensions.k8s.io'
+commands.forEach(command => {
+  describe(`${command} apply crd ${process.env.MOCHA_RUN_TARGET || ''}`, function(this: Common.ISuite) {
+    before(Common.before(this))
+    after(Common.after(this))
 
-  allocateNS(this, ns)
+    const ns: string = createNS()
+    const inNamespace = `-n ${ns}`
+    const crdName = 'crontabs.stable.example.com'
+    const kind = 'CustomResourceDefinition'
+    const apiGroup = 'apiextensions.k8s.io'
 
-  it(`should create custom resource definition from file via "kubectl apply -f"`, async () => {
-    try {
-      console.error('kubectl apply crd 1')
-      const selector = await CLI.command(`kubectl apply -f ${ROOT}/data/k8s/crd.yaml ${inNamespace}`, this.app)
+    allocateNS(this, ns, command)
+
+    it(`should create custom resource definition from file via "${command} apply -f"`, async () => {
+      try {
+        console.error(`${command} apply crd 1`)
+        const selector = await CLI.command(`${command} apply -f ${ROOT}/data/k8s/crd.yaml ${inNamespace}`, this.app)
+          .then(ReplExpect.okWithCustom({ selector: Selectors.BY_NAME(crdName) }))
+          .catch(Common.oops(this))
+
+        // wait for the badge to become green
+        console.error(`${command} apply crd 2`)
+        await waitForGreen(this.app, selector)
+
+        // now click on the table row
+        console.error(`${command} apply crd 3`)
+        await this.app.client.click(`${selector} .clickable`)
+        await SidecarExpect.open(this.app)
+          .then(SidecarExpect.mode(defaultModeForGet))
+          .then(SidecarExpect.showing(crdName))
+
+        let idx = 0
+        await this.app.client.waitUntil(async () => {
+          const text = await Util.getValueFromMonaco(this.app)
+          if (++idx > 5) {
+            console.error(`still waiting for yaml in ${this.title}`, text)
+          }
+
+          return Promise.resolve(text).then(
+            Util.expectYAMLSubset(
+              {
+                kind: 'CronTab'
+              },
+              false
+            )
+          )
+        })
+      } catch (err) {
+        return Common.oops(this, true)(err)
+      }
+    })
+
+    it(`should switch to last applied tab of custom resource definitions`, async () => {
+      try {
+        // make sure we have a last applied tab
+        console.error(`${command} apply crd 4`)
+        await this.app.client.waitForVisible(Selectors.SIDECAR_MODE_BUTTON('last applied'))
+        await this.app.client.click(Selectors.SIDECAR_MODE_BUTTON('last applied'))
+        await this.app.client.waitForVisible(Selectors.SIDECAR_MODE_BUTTON_SELECTED('last applied'))
+
+        let idx = 0
+        console.error(`${command} apply crd 5`)
+        await this.app.client.waitUntil(async () => {
+          const text = await Util.getValueFromMonaco(this.app)
+          if (++idx > 5) {
+            console.error(`still waiting for yaml in ${this.title}`, text)
+          }
+
+          return Promise.resolve(text).then(
+            Util.expectYAMLSubset(
+              {
+                apiVersion: 'apiextensions.k8s.io/v1beta1',
+                kind: 'CustomResourceDefinition',
+                metadata: {
+                  name: crdName
+                }
+              },
+              false
+            )
+          )
+        })
+        console.error(`${command} apply crd 6`)
+      } catch (err) {
+        return Common.oops(this, true)(err)
+      }
+    })
+
+    it('should open crd in sidecar, then click on Show Resources button', async () => {
+      try {
+        const res = await CLI.command(`${command} get ${kind}.${apiGroup} -n ${ns} ${crdName} -o yaml`, this.app)
+
+        console.error(`${command} apply crd 7`)
+        await Promise.resolve(res)
+          .then(ReplExpect.justOK)
+          .then(SidecarExpect.open)
+          .then(SidecarExpect.showing(crdName))
+          .catch(Common.oops(this, true))
+
+        await this.app.client.waitForVisible(Selectors.SIDECAR_MODE_BUTTON('show-crd-resources'))
+        await this.app.client.click(Selectors.SIDECAR_MODE_BUTTON('show-crd-resources'))
+
+        await Promise.resolve({ app: this.app, count: res.count + 1 }).then(
+          ReplExpect.okWithCustom({ selector: Selectors.BY_NAME(crdName) })
+        )
+      } catch (err) {
+        await Common.oops(this, true)(err)
+      }
+    })
+
+    it(`should delete the custom resource definition from URL via ${command}`, () => {
+      return CLI.command(`${command} delete -f ${ROOT}/data/k8s/crd.yaml ${inNamespace}`, this.app)
         .then(ReplExpect.okWithCustom({ selector: Selectors.BY_NAME(crdName) }))
-        .catch(Common.oops(this))
-
-      // wait for the badge to become green
-      console.error('kubectl apply crd 2')
-      await waitForGreen(this.app, selector)
-
-      // now click on the table row
-      console.error('kubectl apply crd 3')
-      await this.app.client.click(`${selector} .clickable`)
-      await SidecarExpect.open(this.app)
-        .then(SidecarExpect.mode(defaultModeForGet))
-        .then(SidecarExpect.showing(crdName))
-
-      let idx = 0
-      await this.app.client.waitUntil(async () => {
-        const text = await Util.getValueFromMonaco(this.app)
-        if (++idx > 5) {
-          console.error(`still waiting for yaml in ${this.title}`, text)
-        }
-
-        return Promise.resolve(text).then(
-          Util.expectYAMLSubset(
-            {
-              kind: 'CronTab'
-            },
-            false
-          )
-        )
-      })
-    } catch (err) {
-      return Common.oops(this, true)(err)
-    }
-  })
-
-  it(`should switch to last applied tab of custom resource definitions`, async () => {
-    try {
-      // make sure we have a last applied tab
-      console.error('kubectl apply crd 4')
-      await this.app.client.waitForVisible(Selectors.SIDECAR_MODE_BUTTON('last applied'))
-      await this.app.client.click(Selectors.SIDECAR_MODE_BUTTON('last applied'))
-      await this.app.client.waitForVisible(Selectors.SIDECAR_MODE_BUTTON_SELECTED('last applied'))
-
-      let idx = 0
-      console.error('kubectl apply crd 5')
-      await this.app.client.waitUntil(async () => {
-        const text = await Util.getValueFromMonaco(this.app)
-        if (++idx > 5) {
-          console.error(`still waiting for yaml in ${this.title}`, text)
-        }
-
-        return Promise.resolve(text).then(
-          Util.expectYAMLSubset(
-            {
-              apiVersion: 'apiextensions.k8s.io/v1beta1',
-              kind: 'CustomResourceDefinition',
-              metadata: {
-                name: crdName
-              }
-            },
-            false
-          )
-        )
-      })
-      console.error('kubectl apply crd 6')
-    } catch (err) {
-      return Common.oops(this, true)(err)
-    }
-  })
-
-  it('should open crd in sidecar, then click on Show Resources button', async () => {
-    try {
-      const res = await CLI.command(`kubectl get ${kind}.${apiGroup} -n ${ns} ${crdName} -o yaml`, this.app)
-
-      console.error('kubectl apply crd 7')
-      await Promise.resolve(res)
-        .then(ReplExpect.justOK)
-        .then(SidecarExpect.open)
-        .then(SidecarExpect.showing(crdName))
+        .then(selector => waitForRed(this.app, selector))
         .catch(Common.oops(this, true))
+    })
 
-      await this.app.client.waitForVisible(Selectors.SIDECAR_MODE_BUTTON('show-crd-resources'))
-      await this.app.client.click(Selectors.SIDECAR_MODE_BUTTON('show-crd-resources'))
-
-      await Promise.resolve({ app: this.app, count: res.count + 1 }).then(
-        ReplExpect.okWithCustom({ selector: Selectors.BY_NAME(crdName) })
-      )
-    } catch (err) {
-      await Common.oops(this, true)(err)
-    }
+    deleteNS(this, ns, command)
   })
-
-  it(`should delete the custom resource definition from URL via kubectl`, () => {
-    return CLI.command(`kubectl delete -f ${ROOT}/data/k8s/crd.yaml ${inNamespace}`, this.app)
-      .then(ReplExpect.okWithCustom({ selector: Selectors.BY_NAME(crdName) }))
-      .then(selector => waitForRed(this.app, selector))
-      .catch(Common.oops(this, true))
-  })
-
-  deleteNS(this, ns)
 })
